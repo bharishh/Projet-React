8905becd2812a9471b9db11ae96bc015a54fc302
bharishh <harish_01@icloud.com>
2025-10-14 17:43:22 +0200

    card

--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -1,12 +1,32 @@
 import "./Components.css"
-function card(props)
+import logo_Montagne from '../assets/montagne.jpg'
+import logo_Koala from '../assets/koala.jpg'
+
+
+function card()
 {
+            const card = [
+            { title: "Nature", image: logo_Montagne},
+            {title: "Animaux", image: logo_Koala}
+            ];
+
     return(
-        <div className="card">
-            <h1>{props.title}</h1>
-            <img  className = "Card"src={props.image} alt=""/>
+        <div>
+            {card.map((item) =>
+            (
+                <>
+                <section><h2>{item.title}</h2></section>
+                
+                    <div className="card">
+                <img src={item.image} alt={item.title} />
+                </div>
+                </>
+            
+
+            ))}
         </div>
     );
+           
 }
 
 export default card;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Card from './Components/Card.jsx'
-import logo from './assets/montagne.jpg'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Card  title ="Nature " image = {logo}/>
-    <Card  image = {logo}/>
+    <Card />
+    
 
   </StrictMode>,
 )
