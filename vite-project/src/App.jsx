23908d835dac060332ee9dde474447ca550c4efd
bharishh <harish_01@icloud.com>
2025-10-14 15:01:42 +0200

    affiche du titre et image

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+    
+    return (
+        <div>
+            <h1>Galerie d'image</h1>
+             </div>
+    )
 }
 
 export default App
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Card from './Components/Card.jsx'
+import logo from './assets/montagne.jpg'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Card  title ="Nature " image = {logo}/>
+    <Card  image = {logo}/>
+
   </StrictMode>,
 )
