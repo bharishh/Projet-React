0c55b76ed2fdefbe790b2480d6261d72a11ac859
MODZINU Kofi Stéphane <stephanemodzinu@gmail.com>
2025-10-14 14:29:02 +0200

    change of vite.config dans add in gitignore

--- vite-project/src/Components/Button.jsx
+++ vite-project/src/Components/Button.jsx
@@ -1,1 +1,13 @@
 // text like dans le bouton 
+import React from 'react';
+
+
+function like(props) {
+        const [count, setCount] = useState(0);
+        return (
+                <button onClick={() => setCount((count) => count + props.value)}>
+                    clique {count + 1} fois
+                </button>
+        );
+}
+export default Button;
