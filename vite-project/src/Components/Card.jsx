11bbc6dff25546fc06029e7c7bf94ecdf7ba3d52
bharishh <harish_01@icloud.com>
2025-10-15 16:01:21 +0200

    goupe images par categorie

--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -15,12 +15,25 @@ function Card() {
     { id: 8, title: "Art", image: "/assets/art-chris.jpg" },
   ];
 
-  return (
-    <section>
-      {card.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-                <img src={item.image} alt={item.title} className="card" />
+
+
+  const grouped = card.reduce((acc, item) => {
+    if (!acc[item.title]) acc[item.title] = [];
+    acc[item.title].push(item.image);
+    return acc;
+  }, {});
+
+
+   return (
+    <section className="categories">
+      {Object.entries(grouped).map(([title, images]) => (
+        <div key={title}>
+          <h2 className="category-title">{title}</h2>
+          <div className="image">
+            {images.map((img, index) => (
+              <img key={index} src={img} alt={title} className="card" />
+            ))}
+          </div>
         </div>
       ))}
     </section>
