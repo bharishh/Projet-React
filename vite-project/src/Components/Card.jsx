0372e5ccca1047a7c753f501bfb7589a26fac17f
bharishh <harish_01@icloud.com>
2025-10-15 15:36:29 +0200

    affichage tout les images

--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -3,9 +3,16 @@ import React from "react";
 
 function Card() {
   const card = [
-    { id: 1, title: "Nature", image: "/assets/montagne.jpg" }
-    , { id: 2, title: "Animaux", image: "/assets/koala.jpg" }
-    , { id: 3, title: "Voiture", image: "/assets/Lambo_yakuza.jpg" }
+    { id: 1, title: "Nature", image: "/assets/montagne.jpg" },
+    { id: 2, title: "Nature", image: "/assets/nature-soleil.jpg" },
+    { id: 3, title: "Animaux", image: "/assets/koala.jpg" },
+    {id : 4, title: "Animaux", image: "/assets/sphynx.jpg" },
+   { id: 3, title: "Voiture", image: "/assets/Lambo_yakuza.jpg" },
+    { id: 4, title: "Voiture", image: "/assets/Old-car-interior.jpg" },
+    { id: 5, title: "Voyage", image: "/assets/voyage-pont.jpg" },
+    { id: 6, title: "Voyage", image: "/assets/Voyage-velo.jpg" },
+    { id: 7, title: "Art", image: "/assets/art-adrianna.jpg" },
+    { id: 8, title: "Art", image: "/assets/art-chris.jpg" },
   ];
 
   return (
